35385820f20eab51e3e4117f6e09448b252c6164
Nagibator2007 <shevchenokse@outlook.com>
2025-12-01 19:11:24 +0500

    fix

--- Tyuiu.ShevchenokSE.Sprint4.Task1.V12.Lib/DataService.cs
+++ Tyuiu.ShevchenokSE.Sprint4.Task1.V12.Lib/DataService.cs
@@ -1,19 +1,19 @@
 ﻿using tyuiu.cources.programming.interfaces.Sprint4;
 namespace Tyuiu.ShevchenokSE.Sprint4.Task1.V12.Lib
 {
-    public class DataService : ISprint4Task1V12 
+    public class DataService : ISprint4Task1V12
     {
         public int Calculate(int[] array)
         {
-            int star = 1;
+            int proiz = 1;
             for (int i = 0; i < array.Length; i++)
             {
-                if (array[i] % 2 == 0)
+                if (array[i] % 2 != 0)
                 {
-                    star *= array[i];
+                    proiz *= array[i];
                 }
             }
-            return star;
+            return proiz;
         }
     }
 }
