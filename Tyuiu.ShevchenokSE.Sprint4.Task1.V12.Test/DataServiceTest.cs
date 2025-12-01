8bc161f406e4a2ca932660c123269d491ba0ec9e
Nagibator2007 <shevchenokse@outlook.com>
2025-12-01 19:08:24 +0500

    task2

---
+++ Tyuiu.ShevchenokSE.Sprint4.Task1.V12.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using Tyuiu.ShevchenokSE.Sprint4.Task1.V12.Lib;
+namespace Tyuiu.ShevchenokSE.Sprint4.Task1.V12.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidGetMultEvenArrEl()
+        {
+            DataService ds = new DataService();
+            int[] array = { 4, 8, 6, 4, 9, 5, 8, 7, 8, 4, 7, 6, 8 };
+            var res = ds.GetMultEvenArrEl(array);
+            int wait = 2205;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
