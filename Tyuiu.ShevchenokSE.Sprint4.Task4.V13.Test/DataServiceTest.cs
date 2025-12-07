5a448548c01cad8a0947648c6a0e4cbb0f122434
Nagibator2007 <shevchenokse@outlook.com>
2025-12-07 22:57:13 +0500

    Task4

---
+++ Tyuiu.ShevchenokSE.Sprint4.Task4.V13.Test/DataServiceTest.cs
@@ -0,0 +1,23 @@
+﻿using Tyuiu.ShevchenokSE.Sprint4.Task4.V13.Lib;
+namespace Tyuiu.ShevchenokSE.Sprint4.Task4.V13.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void Calculate_Test()
+        {
+            DataService dataService = new DataService();
+            int[,] matrix = {
+                {6, 3, 5, 7, 5},
+                {3, 5, 7, 4, 5},
+                {5, 6, 4, 5, 4},
+                {7, 4, 7, 3, 3},
+                {4, 6, 5, 5, 6}
+            };
+            int expectedSum = 44;
+            int actualSum = dataService.Calculate(matrix);
+            Assert.AreEqual(expectedSum, actualSum);
+        }
+    }
+}
---
+++ Tyuiu.ShevchenokSE.Sprint4.Task4.V13/Program.cs
@@ -0,0 +1,45 @@
+﻿using Tyuiu.ShevchenokSE.Sprint4.Task4.V13.Lib;
+namespace Tyuiu.ShevchenokSE.Sprint4.Task4.V13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService dataService = new DataService();
+            Console.Title = "Спринт #4 | Выполнил: Шевченок С. Е. | СМАРТб-25-1";
+            Console.WriteLine("************************************************************************");
+            Console.WriteLine("* Спринт #4                                                            *");
+            Console.WriteLine("* Тема: Двумерные массивы(ввод с клавиатуры)                           *");
+            Console.WriteLine("* Задание #4                                                           *");
+            Console.WriteLine("* Вариант #13                                                          *");
+            Console.WriteLine("* Выполнил: Шевченок Сергей Евгеньевич | СМАРТб-25-1                   *");
+            Console.WriteLine("************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                             *");
+            Console.WriteLine("* Дан двумерный целочисленный массив 5 на 5 элементов, заполненный     *");
+            Console.WriteLine("* значениями с клавиатуры в диапазоне от 3 до 7.                       *");
+            Console.WriteLine("* Найдите сумму четных элементов массива.                              *");
+            Console.WriteLine("*                                                                      *");
+            Console.WriteLine("************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                     *");
+            Console.WriteLine("* 6, 3, 5, 7, 5,                                                       *");
+            Console.WriteLine("* 3, 5, 7, 4, 5,                                                       *");
+            Console.WriteLine("* 5, 6, 4, 5, 4,                                                       *");
+            Console.WriteLine("* 7, 4, 7, 3, 3,                                                       *");
+            Console.WriteLine("* 4, 6, 5, 5, 6,                                                       *");
+            Console.WriteLine("************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                           *");
+
+            int[,] matrix = {
+                    {6, 3, 5, 7, 5},
+                    {3, 5, 7, 4, 5},
+                    {5, 6, 4, 5, 4},
+                    {7, 4, 7, 3, 3},
+                    {4, 6, 5, 5, 6}
+            };
+            int sumOfEvenElements = dataService.Calculate(matrix);
+            Console.WriteLine($"Сумма четных элементов: {sumOfEvenElements}");
+            Console.WriteLine("************************************************************************");
+            Console.ReadLine();
+        }
+    }
+}
