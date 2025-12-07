9396001af3223511ea4b818263ef94c7209911f3
Nagibator2007 <shevchenokse@outlook.com>
2025-12-08 00:04:15 +0500

    task6

---
+++ Tyuiu.ShevchenokSE.Sprint4.Task6.V29.Lib/DataService.cs
@@ -0,0 +1,12 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint4;
+namespace Tyuiu.ShevchenokSE.Sprint4.Task6.V29.Lib
+{
+    public class DataService : ISprint4Task6V29
+    {
+        public int Calculate(string[] array)
+        {
+            int count = array.Count(x => x.Length > 4);
+            return count;
+        }
+    }
+}
---
+++ Tyuiu.ShevchenokSE.Sprint4.Task6.V29/Program.cs
@@ -0,0 +1,34 @@
+﻿using Tyuiu.ShevchenokSE.Sprint4.Task6.V29.Lib;
+namespace Tyuiu.ShevchenokSE.Sprint4.Task6.V29
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #4 | Выполнил Божуков В. П. | ИБКСб-24-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #4                                                               *");
+            Console.WriteLine("* Задание #6                                                              *");
+            Console.WriteLine("* Вариант #29                                                             *");
+            Console.WriteLine("* Выполнил: Божуков Владимир Павлович   | ИБКСб-24-1                      *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Дан строковый массив данных                                             *");
+            Console.WriteLine("* используя класс Array подсчитайте количество элементов,                 *");
+            Console.WriteLine("* длина которых больше 4.                                                 *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("Найти слова, содержащие больше 4 букв, в массиве:");
+            Console.WriteLine("\"Весна\", \"Лето\", \"Осень\", \"Зима\"");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            string[] array = ["Весна", "Лето", "Осень", "Зима"];
+            int z = ds.Calculate(array);
+            Console.WriteLine(z);
+            Console.ReadKey();
+        }
+    }
+}
