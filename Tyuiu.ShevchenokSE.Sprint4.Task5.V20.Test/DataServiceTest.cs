d927d7a40d788e51668bb4ef9fea90829840f725
Nagibator2007 <shevchenokse@outlook.com>
2025-12-07 23:40:54 +0500

    Task5

---
+++ Tyuiu.ShevchenokSE.Sprint4.Task5.V20.Test/DataServiceTest.cs
@@ -0,0 +1,21 @@
+﻿using Tyuiu.ShevchenokSE.Sprint4.Task5.V20.Lib;
+namespace Tyuiu.ShevchenokSE.Sprint4.Task5.V20.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            int[,] mas = new int[5, 5] {{-4, -5, -5, -6, -4},
+                                     { -7, -8, -4, -7,- 5},
+                                     {-5, -6, -5, -8, -5},
+                                     { -7, -5, -8, -7, -8},
+                                     { -4, -7, -7, 8, 8} };
+            int res = ds.Calculate(mas);
+            int wait = 16;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.ShevchenokSE.Sprint4.Task5.V20/Program.cs
@@ -0,0 +1,54 @@
+﻿using Tyuiu.ShevchenokSE.Sprint4.Task5.V20.Lib;
+namespace Tyuiu.ShevchenokSE.Sprint4.Task5.V20
+{
+    class Programm
+    {
+    private static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Random rnd = new Random();
+            Console.Title = "Спринт #4 | Выполнил: Шевченок С. Е. | СМАРТб-25-1";
+            Console.WriteLine("************************************************************************");
+            Console.WriteLine("* Спринт #4                                                            *");
+            Console.WriteLine("* Тема: Двумерные массивы. (генератор случайных чисел)                 *");
+            Console.WriteLine("* Задание #5                                                           *");
+            Console.WriteLine("* Вариант #20                                                          *");
+            Console.WriteLine("* Выполнил: Шевченок Сергей Евгеньевич | СМАРТб-25-1                   *");
+            Console.WriteLine("************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                             *");
+            Console.WriteLine("* Найти сумму положительных элементов.                                 *");
+            Console.WriteLine("************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                     *");
+            Console.WriteLine("************************************************************************");
+            Console.Write("Введите количество строк в массиве:");
+            int rows = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите количество столбцов в массиве:");
+            int columns = Convert.ToInt32(Console.ReadLine());
+            int[,] matrix = new int[rows, columns];
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+
+                    matrix[i, j] = rnd.Next(-7, 3);
+                }
+            }
+            Console.WriteLine("\nМассив:");
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    Console.Write($"{matrix[i, j]}\t");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            int res = ds.Calculate(matrix);
+            Console.WriteLine("Сумма чётных элементов-" + res);
+            Console.ReadKey();
+        }
+    }
+}
